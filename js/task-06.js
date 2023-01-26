e045d127dab891bf0e1cccf4a00a57eb0347646c
AniutkinyRukodelki <anyageiko91@gmail.com>
2023-01-26 15:53:22 +0200

    correct

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,44 @@
-const input = document.querySelector("#validation-input");
+const inputEl = document.querySelector("#validation-input");
 
-input.addEventListener("blur", checkSumbols);
+inputEl.addEventListener("blur", checkSumbols);
 
-function checkSumbols() {
-  if (input.value.length === Number(input.dataset.length)) {
-    input.classList.add("valid");
-    input.classList.remove("invalid");
-  } else {
-    input.classList.add("invalid");
-    input.classList.remove("valid");
+const valueFor = Number(inputEl.dataset.length);
+
+function checkSumbols(i) {
+  inputEl.textContent = i.currentTarget.value;
+
+  if (valueFor === Number(inputEl.textContent.length)) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else if (
+    valueFor < Number(inputEl.textContent.length) ||
+    valueFor > Number(inputEl.textContent.length)
+  ) {
+    inputEl.classList.add("invalid");
   }
 }
+// Зробила трохи по іншому. По іншому воно в мене не спрацьовує
 
+
+
+
+
+// const input = document.querySelector("#validation-input");
+
+// input.addEventListener("blur", checkSumbols);
+
+// function checkSumbols() {
+//   if (input.value.length === Number(input.dataset.length)) {
+//     input.classList.add("valid");
+//     input.classList.remove("invalid");
+//   } else {
+//     input.classList.add("invalid");
+//     input.classList.remove("valid");
+//   }
+// }
+
+
+// const input = document.querySelector("#validation-input");
 // const lengthEl = Number(input.dataset.length);
 
 // input.addEventListener('input', onInputBlur);
